38eceab3a9f2b1b399739dcf1df53bf1ee180d0f
Qnicery <nicefirefly@mail.ru>
2025-11-07 13:40:19 +0300

    пр7

---
+++ lib/features/hotels/screens/history_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../models/booking.dart';
+
+class HistoryScreen extends StatelessWidget {
+  final List<Booking> history;
+
+  const HistoryScreen({super.key, required this.history});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('История бронирования', style: TextStyle(fontWeight: FontWeight.bold)), centerTitle: true),
+      body: history.isEmpty
+          ? const Center(child: Text('Вы пока ничего не бронировали'))
+          : ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: history.length,
+        itemBuilder: (context, index) {
+          final booking = history[index];
+          return Card(
+            elevation: 2,
+            margin: const EdgeInsets.only(bottom: 12),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          booking.hotelName,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text('Город: ${booking.city}', style: TextStyle(color: Colors.grey[600])),
+                        Text('Гостей: ${booking.guests}', style: TextStyle(color: Colors.grey[600])),
+                        Text('Дата: ${booking.date.day}.${booking.date.month}.${booking.date.year}', style: TextStyle(color: Colors.grey[600])),
+                      ],
+                    ),
+                  ),
+                  Text('${booking.price} ₽', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold,)),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 1,
+          onTap: (i) {
+            switch (i) {
+              case 0:
+                context.go('/hotels', extra: history);
+                break;
+              case 2:
+                context.go('/profile', extra: history);
+                break;
+            }
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Главная'),
+            BottomNavigationBarItem(icon: Icon(Icons.history), label: 'История'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Профиль'),
+          ],
+        )
+
+    );
+  }
+}
